2a4d03df4c522cbc215fa88b269c146328daf3a4
Florian Gérard <kingdomflo16@gmail.com>
2020-01-31 13:41:57 +0100

    just rework indent

--- test/relationship.test.js
+++ test/relationship.test.js
@@ -1,139 +1,135 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 // TODO do it with a direct require server and not hardcoded URL
 // let server = require('../');
-let baseUrl = chai.request('http://localhost:8100');
+let baseUrl = chai.request("http://localhost:8100");
 
 let relationshipId;
 
-describe('Relationship route', function (done) {
-
-    it(' /relationship it should have an error because we are not authenticated', function (done) {
-        baseUrl
-            .get('/relationship')
-            .end((err, res) => {
-                res.should.have.status(401);
-                done();
-            });
+describe("Relationship route", function(done) {
+  it(" /relationship it should have an error because we are not authenticated", function(done) {
+    baseUrl.get("/relationship").end((err, res) => {
+      res.should.have.status(401);
+      done();
     });
+  });
 
-    it(' /relationship it should GET all the relationship from a user, 2', function (done) {
-        baseUrl
-            .get('/relationship')
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(2);
-                done();
-            });
-    });
+  it(" /relationship it should GET all the relationship from a user, 2", function(done) {
+    baseUrl
+      .get("/relationship")
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(2);
+        done();
+      });
+  });
 
-    it(' /relationship it should not POST a new relationship', function (done) {
-        baseUrl
-            .post('/relationship')
-            .set('Authorization', process.env.TOKEN)
-            .send({ name: "", userId: 2 })
-            .end((err, res) => {
-                res.should.have.status(422);
-                res.body.should.have.property('messages');
-                done();
-            });
-    });
+  it(" /relationship it should not POST a new relationship", function(done) {
+    baseUrl
+      .post("/relationship")
+      .set("Authorization", process.env.TOKEN)
+      .send({ name: "", userId: 2 })
+      .end((err, res) => {
+        res.should.have.status(422);
+        res.body.should.have.property("messages");
+        done();
+      });
+  });
 
-    it(' /relationship it should not POST a new relationship', function (done) {
-        baseUrl
-            .post('/relationship')
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(422);
-                res.body.should.have.property('messages');
-                done();
-            });
-    });
+  it(" /relationship it should not POST a new relationship", function(done) {
+    baseUrl
+      .post("/relationship")
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(422);
+        res.body.should.have.property("messages");
+        done();
+      });
+  });
 
-    it(' /relationship it should POST a new relationship', function (done) {
-        baseUrl
-            .post('/relationship')
-            .set('Authorization', process.env.TOKEN)
-            .send({ name: "John Doe" })
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.have.property('id');
-                res.body.should.have.property('name');
-                relationshipId = res.body.id;
-                done();
-            });
-    });
+  it(" /relationship it should POST a new relationship", function(done) {
+    baseUrl
+      .post("/relationship")
+      .set("Authorization", process.env.TOKEN)
+      .send({ name: "John Doe" })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("id");
+        res.body.should.have.property("name");
+        relationshipId = res.body.id;
+        done();
+      });
+  });
 
-    it(' /relationship it should GET all the relationship from a user, now 3', function (done) {
-        baseUrl
-            .get('/relationship')
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(3);
-                done();
-            });
-    });
+  it(" /relationship it should GET all the relationship from a user, now 3", function(done) {
+    baseUrl
+      .get("/relationship")
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(3);
+        done();
+      });
+  });
 
-    it(' /relationship it should GET the 3th relationship, John Doe', function (done) {
-        baseUrl
-            .get('/relationship/' + relationshipId)
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.have.property('id').eql(relationshipId);
-                res.body.should.have.property('name').eql('John Doe');
-                done();
-            });
-    });
+  it(" /relationship it should GET the 3th relationship, John Doe", function(done) {
+    baseUrl
+      .get("/relationship/" + relationshipId)
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("id").eql(relationshipId);
+        res.body.should.have.property("name").eql("John Doe");
+        done();
+      });
+  });
 
-    it(' /relationship it should not DELETE the relationship id one wich is not belong to the current user', function (done) {
-        baseUrl
-            .delete('/relationship/1')
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(400);
-                done();
-            });
-    });
+  it(" /relationship it should not DELETE the relationship id one wich is not belong to the current user", function(done) {
+    baseUrl
+      .delete("/relationship/1")
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
 
-    it(' /relationship it should DELETE the relationship John Doe', function (done) {
-        baseUrl
-            .delete('/relationship/' + relationshipId)
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
-    });
+  it(" /relationship it should DELETE the relationship John Doe", function(done) {
+    baseUrl
+      .delete("/relationship/" + relationshipId)
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 
-    it(' /relationship it should GET all the relationship from a user, now 2', function (done) {
-        baseUrl
-            .get('/relationship')
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(2);
-                done();
-            });
-    });
+  it(" /relationship it should GET all the relationship from a user, now 2", function(done) {
+    baseUrl
+      .get("/relationship")
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(2);
+        done();
+      });
+  });
 
-    it(' /relationship it should not DELETE the relationship wich is not present anymore', function (done) {
-        baseUrl
-            .delete('/relationship/' + relationshipId)
-            .set('Authorization', process.env.TOKEN)
-            .end((err, res) => {
-                res.should.have.status(400);
-                done();
-            });
-    });
-
+  it(" /relationship it should not DELETE the relationship wich is not present anymore", function(done) {
+    baseUrl
+      .delete("/relationship/" + relationshipId)
+      .set("Authorization", process.env.TOKEN)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
 });
--- test/user.test.js
+++ test/user.test.js
@@ -1,49 +1,45 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 // TODO do it with a direct require server and not hardcoded URL
 // let server = require('../');
-let baseUrl = chai.request('http://localhost:8100');
+let baseUrl = chai.request("http://localhost:8100");
 
 let token;
 
-describe('User route', function (done) {
-
-  it(' /login it should GET the token', function (done) {
+describe("User route", function(done) {
+  it(" /login it should GET the token", function(done) {
     baseUrl
-      .post('/login').send({ authId: "auth02|aivanidnacs" })
+      .post("/login")
+      .send({ authId: "auth02|aivanidnacs" })
       .end((err, res) => {
         res.should.have.status(200);
-        res.body.should.have.property('apiToken');
+        res.body.should.have.property("apiToken");
         process.env.TOKEN = res.body.apiToken;
         done();
       });
   });
 
-  it(' /user it should GET all the user', function (done) {
-    baseUrl
-      .get('/user')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(2);
-        done();
-      });
+  it(" /user it should GET all the user", function(done) {
+    baseUrl.get("/user").end((err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.a("array");
+      res.body.length.should.be.eql(2);
+      done();
+    });
   });
 
-  it(' /user/:id it should GET all the user', function (done) {
+  it(" /user/:id it should GET all the user", function(done) {
     const id = 1;
-    baseUrl
-      .get('/user/' + id)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('id').eql(id);
-        res.body.should.have.property('name');
-        done();
-      });
+    baseUrl.get("/user/" + id).end((err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.a("object");
+      res.body.should.have.property("id").eql(id);
+      res.body.should.have.property("name");
+      done();
+    });
   });
 });
